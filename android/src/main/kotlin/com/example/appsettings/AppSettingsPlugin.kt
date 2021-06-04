bab409431d19573fb702ebe66ff46c355e7f6316
Julian Sharon <julian.s@grootan.com>
2021-06-04 13:07:07 +0530

    made some formatting changes

--- android/src/main/kotlin/com/example/appsettings/AppSettingsPlugin.kt
+++ android/src/main/kotlin/com/example/appsettings/AppSettingsPlugin.kt
@@ -114,9 +114,8 @@ class AppSettingsPlugin() : MethodCallHandler, FlutterPlugin, ActivityAware {
         } else if (call.method == "app_settings") {
             openAppSettings(asAnotherTask)
         }
-        else if (call.method=="device_settings")
-        {
-            openSettings(Settings.ACTION_SETTINGS, asAnotherTask);
+        else if (call.method == "device_settings") {
+            openSettings(Settings.ACTION_SETTINGS, asAnotherTask)
         }
     }
 }
