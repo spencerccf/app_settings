cfc951f72e6439d8a950e4773e7d88a1a8fe77e5
Navaron Bracke <brackenavaron@gmail.com>
2023-05-05 20:15:07 +0200

    format swift impl; support openNotificationSettingsURLString on iOS 16

--- ios/Classes/AppSettingsPlugin.swift
+++ ios/Classes/AppSettingsPlugin.swift
@@ -2,24 +2,36 @@ import Flutter
 import UIKit
 
 public class AppSettingsPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "app_settings", binaryMessenger: registrar.messenger())
-    let instance = AppSettingsPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    openSettings()
-    result(nil)
-  }
-
-  private func openSettings() {
-    if let url = URL(string: UIApplication.openSettingsURLString) {
-      if #available(iOS 10.0, *) {
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-      } else {
-        UIApplication.shared.openURL(url)
-      }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "app_settings", binaryMessenger: registrar.messenger())
+        let instance = AppSettingsPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        var url: String = UIApplication.openSettingsURLString
+        
+        switch(call.method) {
+        case "notification":
+            if #available(iOS 16.0, *) {
+                url = UIApplication.openNotificationSettingsURLString
+            }
+            break
+        default:
+            break
+        }
+        
+        openSettings(settingsUrl: url)
+        result(nil)
+    }
+    
+    private func openSettings(settingsUrl: String) {
+        if let url = URL(string: settingsUrl) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
 }
