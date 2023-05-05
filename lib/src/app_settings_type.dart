a9410a706abbfac00a4fb67158e08cee0a593ea5
Navaron Bracke <brackenavaron@gmail.com>
2023-05-05 18:07:11 +0200

    implement enum for settings types

---
+++ lib/src/app_settings_type.dart
@@ -0,0 +1,65 @@
+/// This enum defines the different setting types.
+enum AppSettingsType {
+  /// Open the accessibility settings.
+  accessibility,
+
+  /// Open the alarm settings.
+  alarm,
+
+  /// Open the APN settings.
+  apn,
+
+  /// Open the Battery Optimization settings.
+  batteryOptimization,
+
+  /// Open the Bluetooth settings.
+  bluetooth,
+
+  /// Open the Data Roaming settings.
+  dataRoaming,
+
+  /// Open the date settings.
+  date,
+
+  /// Open the Developer settings.
+  developer,
+
+  /// Open the device settings.
+  device,
+
+  /// Open the display settings.
+  display,
+
+  /// Open the Hotspot settings.
+  hotspot,
+
+  /// Open the internal storage settings.
+  internalStorage,
+
+  /// Open the location settings.
+  location,
+
+  /// Open the Lock And Password settings.
+  lockAndPassword,
+
+  /// Open the NFC settings.
+  nfc,
+
+  /// Open the notification settings.
+  notification,
+
+  /// Open the security settings.
+  security,
+
+  /// Open the audio settings.
+  sound,
+
+  /// Open the VPN settings.
+  vpn,
+
+  /// Open the wifi settings.
+  wifi,
+
+  /// Open the wireless settings.
+  wireless,
+}
